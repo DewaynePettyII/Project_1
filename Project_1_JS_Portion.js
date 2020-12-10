56e9658ed8abed6afe495e52512377fb38e0344b
Dewayne Petty II <christsiblings@gmail.com>
2020-12-10 07:29:15 -0600

    removing comments

--- Project_1_JS_Portion.js
+++ Project_1_JS_Portion.js
@@ -12,40 +12,30 @@ function listLength(){
 }
 
 function createListElement() {
-	var li = document.createElement("li"); // creates an element "li"
-	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
-	input.value = ""; //Reset text input field
+	var li = document.createElement("li"); 
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li); 
+	input.value = "";
 
-
-	//START STRIKETHROUGH
-	// because it's in the function, it only adds it for new items
 	function crossOut() {
 		li.classList.toggle("done");
 	}
 
 	li.addEventListener("click",crossOut);
-	//END STRIKETHROUGH
+	
 
-
-	// START ADD DELETE BUTTON
 	var dBtn = document.createElement("button");
 	dBtn.appendChild(document.createTextNode("X"));
 	li.appendChild(dBtn);
 	dBtn.addEventListener("click", deleteListItem);
-	// END ADD DELETE BUTTON
-
-
-	//ADD CLASS DELETE (DISPLAY: NONE)
+	
 	function deleteListItem(){
 		li.classList.add("delete")
 	}
-	//END ADD CLASS DELETE
 }
 
-
 function addListAfterClick(){
-	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
+	if (inputLength() > 0) {\
 		createListElement();
 	}
 }
